9e286cffc151307a1c55258de9a9abcd28c6798b
justinsg04 <gloverj820@gmail.com>
2024-10-23 16:03:27 -0400

    oct 23

--- js/arith.js
+++ js/arith.js
@@ -1,12 +1,20 @@
 // get the element 
+let spanE1 = document.getElementById('num1');
+console.log("spanE1 -  " + spanE1);
 
+let spanE2 = document.getElementById('num2');
+console.log("spanE2 -  " + spanE2);
 // element --- property
-var num1 = parseInt(document.getElementById('n1').textContent); 
+let n1 = parseInt(spanE1.textContent);
+let n2 = parseInt(spanE2.textContent);
+console.log(typeof n1);
+console.log(typeof n2);
+//var num1 = parseInt(document.getElementById('n1').textContent); 
 
 
 
 //process
-var c = num1 + num2;
+var c = n1 + n2;
 
 //display
 document.write(c);
